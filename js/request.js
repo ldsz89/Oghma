c61f259e5831719ef8ba80b2391f57316c6d12f6
Dre Shannon <ldsz89@mail.missouri.edu>
2017-11-14 09:37:34 -0600

    Added modal and API queries

--- js/request.js
+++ js/request.js
@@ -1,18 +1,1 @@
-function requestResource(url) {
-  var data = null;
 
-  var xhr = new XMLHttpRequest();
-  xhr.withCredentials = true;
-
-  xhr.addEventListener("readystatechange", function () {
-    if (this.readyState === 4) {
-      console.log(this.responseText);
-    }
-  });
-
-  // xhr.open("GET", "http://www.dnd5eapi.co/api/classes/");
-  xhr.open("GET", url);
-  xhr.withCredentials = false;
-
-  xhr.send(data);
-}
